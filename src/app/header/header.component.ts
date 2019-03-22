ae91d8b5235a57aef303d5353e9ede0119c36484
vkarpeniuk <vasylkarpeniuk@gmail.com>
2019-03-22 08:28:25 +0200

    heroku related changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { environment } from "./../../environments/environment";
+import { environment } from "../../environments/environment";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
---
+++ src/server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/find-and-go"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/find-and-go/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+alert(process.env.testvar);
