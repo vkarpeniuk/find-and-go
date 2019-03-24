8d0ebfb81886ec3f709d454152c20054bcc5101b
vkarpeniuk <vasylkarpeniuk@gmail.com>
2019-03-24 11:29:22 +0200

    added heroku headers

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Accept: 'application/vnd.heroku+json; version=3'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +15,9 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getTestVar(): Observable<any> {
-    return this.http.get('https://api.heroku.com/apps/find-and-go/config-vars');
+    return this.http.get(
+      'https://api.heroku.com/apps/find-and-go/config-vars',
+      httpOptions
+    );
   }
 }
