fd519c0ea9365db5e39202600e966565489d1744
vkarpeniuk <vasylkarpeniuk@gmail.com>
2019-06-24 09:41:41 +0300

    add enter animation to venues list items make logo a link to start page

--- src/app/modules/main/components/venues-list/venues-list.component.ts
+++ src/app/modules/main/components/venues-list/venues-list.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 import { Venue } from '@models';
 
 @Component({
   selector: 'app-venues-list',
   templateUrl: './venues-list.component.html',
-  styleUrls: ['./venues-list.component.scss']
+  styleUrls: ['./venues-list.component.scss'],
+  animations: [
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),
+        animate(
+          '1s cubic-bezier(.8, -0.6, 0.2, 1.5)',
+          style({ transform: 'scale(1)', opacity: 1 })
+        )
+      ])
+    ])
+  ]
 })
 export class VenuesListComponent implements OnInit {
   @Input() venues: Venue[];
