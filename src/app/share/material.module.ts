872414bb5c475f8ecec907822265af515e3edebe
vkarpeniuk <vasylkarpeniuk@gmail.com>
2019-07-28 14:04:23 +0300

    update angular material

--- src/app/share/material.module.ts
+++ src/app/share/material.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
-import {
-  MatAutocompleteModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatOptionModule,
-  MatListModule,
-  MatProgressSpinnerModule,
-  MatProgressBarModule,
-  MatSnackBarModule,
-  MAT_SNACK_BAR_DEFAULT_OPTIONS,
-  MatButtonModule,
-  MatBadgeModule,
-  MatDividerModule,
-  MatCardModule,
-  MatDialogModule
-} from '@angular/material';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
